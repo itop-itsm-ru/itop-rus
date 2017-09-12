23da0356c5365723a2376284f098516a846b31a5
Vladimir Kunin <v.b.kunin@gmail.com>
2017-09-12 12:58:19 +0300

    Модуль готовых ответов

---
+++ extensions/precanned-replies-pro/ru.dict.precanned-replies-pro.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Localized data
+ *
+ * @author      Vladimir Kunin <v.b.kunin@gmail.com>
+ * @link        http://community.itop-itsm.ru  iTop Russian Community
+ * @link        https://github.com/itop-itsm-ru/itop-rus
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Menu:PrecannedRepliesCategories' => 'Категории готовых ответов',
+	'Menu:PrecannedRepliesCategories+' => 'Категории готовых ответов',
+
+	'Class:PrecannedReplyCategory' => 'Категория готовых ответов',
+	'Class:PrecannedReplyCategory+' => 'Категория готовых ответов',
+	'Class:PrecannedReplyCategory/Attribute:name' => 'Название',
+	'Class:PrecannedReplyCategory/Attribute:name+' => '',
+	'Class:PrecannedReplyCategory/Attribute:description' => 'Описание',
+	'Class:PrecannedReplyCategory/Attribute:description+' => '',
+	'Class:PrecannedReplyCategory/Attribute:replies_list' => 'Готовые ответы',
+	'Class:PrecannedReplyCategory/Attribute:replies_list+' => 'Готовые ответы в этой категории',
+
+	'Class:PrecannedReply/Attribute:org_id' => 'Организация',
+	'Class:PrecannedReply/Attribute:org_id+' => '',
+	'Class:PrecannedReply/Attribute:category_id' => 'Категория',
+	'Class:PrecannedReply/Attribute:category_id+' => '',
+));
---
+++ extensions/precanned-replies/ru.dict.precanned-replies.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Localized data
+ *
+ * @author      Vladimir Kunin <v.b.kunin@gmail.com>
+ * @link        http://community.itop-itsm.ru  iTop Russian Community
+ * @link        https://github.com/itop-itsm-ru/itop-rus
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Menu:PrecannedReplies' => 'Готовые ответы',
+	'Menu:PrecannedReplies+' => 'Готовые ответы',
+
+	'UI:Button-AddReply' => 'Готовые ответы...',
+	'UI:Dlg-PickAReply' => 'Выберите ответ',
+	
+	'Class:PrecannedReply' => 'Готовый ответ',
+	'Class:PrecannedReply+' => 'Готовый ответ',
+	'Class:PrecannedReply/Attribute:name' => 'Название',
+	'Class:PrecannedReply/Attribute:name+' => '',
+	'Class:PrecannedReply/Attribute:description' => 'Описание',
+	'Class:PrecannedReply/Attribute:description+' => '',
+	'Class:PrecannedReply/Attribute:body' => 'Текст',
+	'Class:PrecannedReply/Attribute:body+' => '',
+));
